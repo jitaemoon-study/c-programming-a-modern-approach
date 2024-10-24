0baf0621d7e64792ce594040982f8875fa2d1ea2
jitae5moon <jitaemoon@knou.ac.kr>
2024-10-24 00:38:28 -0400

    Chapter04 Expressions

---
+++ chapter04/operators.c
@@ -0,0 +1,6 @@
+/*
+ * Assignment(=) is an operator.
+ * Operators have side effects if they modify operands.(-, ++, --)
+ * Assignment operator needs lvalue as its left operand.
+ * Lvalue is an object stored in computer memory like variables.
+ */
